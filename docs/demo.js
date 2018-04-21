a91f36d5f46be710f4f102a70ca42e574b77c1d9
VooDooS <thevoodoos@gmail.com>
2018-04-21 23:28:07 +0200

    Better demo

--- docs/demo.js
+++ docs/demo.js
@@ -2,10 +2,14 @@
   
   function log(l, p, t) { 
     console.log(l, p, t);
+    child = document.createElement("div");
+    child.innerHTML = "Log (" + l + ") : [ " + p + "] : " + t;
+    document.getElementById("console")
+    .appendChild(child)
   }
 
   function answer(arg, ass) {
-    console.log(arg, ass);
+    log("answer", "",  JSON.stringify(arg, null, 4) +  JSON.stringify(ass, null, 4));
   }
   
   const elp = new Elpi(log, answer);
