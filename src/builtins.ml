eb92a80ca35024da09effb77d6bd25ffd0ba53e1
VooDooS <thevoodoos@gmail.com>
2018-05-03 14:13:27 +0200

    Work in progress

--- src/builtins.ml
+++ src/builtins.ml
@@ -31,4 +31,26 @@ let make () =
           fun p m ~depth:_ -> Log.error m ~prefix:p
         ),
         DocAbove 
+      );
+      MLCode(
+        Pred("js_types",
+          In(list any, "LN",
+            In(list any, "LT", 
+              Easy "js_types LT sends the list of types LT to the OCaml worker (used during statick check).")),
+          fun ln lt ~depth:_ -> 
+            let open Elpi_API.Extend.Data in
+            let lt = List.map2 (fun n t -> 
+              let name = (match (Elpi_API.Extend.Data.look 0 n) with
+                        | CData(c) -> Elpi_API.Extend.Data.C.to_string c
+                        | _ -> failwith "Not a string") 
+              in
+              Format.pp_print_string (Format.str_formatter) name;
+              Format.pp_print_string (Format.str_formatter) " ==> ";
+              Elpi_API.Extend.Pp.term 0 (Format.str_formatter) t;
+              Format.flush_str_formatter ()) ln lt
+            in
+            
+          Firebug.console##log (Js.array (Array.of_list lt))
+        ),
+        DocAbove 
       )] @ Elpi_builtin.std_declarations)
